365ccb6bc8fcbb00ee75895527657b3c86e9cd97
Alisson Santana <alissonsantana16608@gmail.com>
2023-04-12 15:06:51 -0300

    Personalizando o Dashboard

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -3,8 +3,9 @@ import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
 import { sequelize } from '../database'
 import { adminJsResources } from './resources'
-import { User } from '../models'
 import bcrypt from 'bcrypt'
+import { locale } from './locale'
+import { Category,Course, Episode, User } from '../models'
 
 AdminJs.registerAdapter(AdminJsSequelize)
 
@@ -32,6 +33,23 @@ export const adminJs = new AdminJs({
       hoverBg: '#151515',
       }
     }
+  },
+  locale: locale,
+  dashboard: {
+    component: AdminJs.bundle('./components/Dashboard'),
+		handler: async (req, res, context) => {
+      const courses = await Course.count()
+      const episodes = await Episode.count()
+      const categories = await Category.count()
+      const standardUsers = await User.count({ where: { role: 'user' } })
+
+      res.json({
+        'Cursos': courses,
+        'Episódios': episodes,
+        'Categorias': categories,
+        'Usuários': standardUsers
+      })
+    },
   }
 })
 
