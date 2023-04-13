d0bcc75c3e218b60ba5bbc39d7636fde71037e1e
Alisson Santana <alissonsantana16608@gmail.com>
2023-04-13 10:50:32 -0300

    Obtendo 3 cursos em destaque

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -9,7 +9,7 @@ export const courseService = {
         'synopsis', 
         ['thumbnail_url', 'thumbnailUrl']],
       include: {
-        association: 'Episodes',
+        association: 'episodes',
         attributes: [
           'id',
           'name',
@@ -24,5 +24,22 @@ export const courseService = {
     })
 
     return courseWithEpisodes
+  },
+  
+  getRandomFeaturedCourses: async () => {
+    const featuredCourses = await Course.findAll({
+      attributes: [
+        'id', 
+        'name', 
+        'synopsis', 
+        ['thumbnail_url', 'thumbnailUrl']],
+      where: {
+        featured: true
+      }
+    })
+
+    const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
+
+    return randomFeaturedCourses.slice(0, 3)
   },
 }
