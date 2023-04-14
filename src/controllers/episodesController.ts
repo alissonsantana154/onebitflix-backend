6bfb4033a887d8f1bbe618ad2e087668a33d8d25
Alisson Santana <alissonsantana16608@gmail.com>
2023-04-14 09:40:59 -0300

    Streaming de Vídeo na API

---
+++ src/controllers/episodesController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express'
+import { episodeService } from '../services/episodeService'
+
+export const episodesController = {
+  // GET /episodes/stream
+  stream: async (req: Request, res: Response) => {
+    const { videoUrl } = req.query
+    const range = req.headers.range
+
+    try {
+      if (typeof videoUrl !== 'string') {
+        throw new Error('videoUrl must be of type \'string\'');
+      }
+
+      episodeService.streamEpisodeToResponse(res, videoUrl, range)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { categoriesController } from "./controllers/categoriesController"
 import { coursesController } from "./controllers/coursesController"
+import { episodesController } from "./controllers/episodesController"
 
 const router = express.Router()
 
@@ -12,5 +13,6 @@ router.get('/courses/newest', coursesController.newest)
 router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
 
+router.get('/episodes/stream', episodesController.stream)
 
 export { router }
