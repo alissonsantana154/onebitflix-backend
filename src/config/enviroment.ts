912ba80f893e68c61159ec3affe755d0020f2429
Alisson Santana <alissonsantana16608@gmail.com>
2023-08-22 11:53:56 -0300

    restarting the project

--- src/config/enviroment.ts
+++
@@ -1,8 +0,0 @@
-import * as env from "env-var"
-
-export const DATABASE_URL = env.get("DATABASE_URL").required().asString()
-
-export const ADMINJS_COOKIE_PASSWORD = env.get("ADMINJS_COOKIE_PASSWORD").required().asString()
-
-export const JWT_key = env.get("JWT_KEY").required().asString()
-
--- src/database/index.ts
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize'
-import { DATABASE_URL } from '../config/enviroment'
-
-export const sequelize = new Sequelize(DATABASE_URL, {
-	define: {
-    underscored: true
-    }
-})
